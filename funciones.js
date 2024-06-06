60a4331928f68baabce028d3951fd6ab8bd28f25
Samuel Rivero <jsamiju13@gmail.com>
2024-06-06 19:22:30 -0400

    implementación menor correcta de js

--- funciones.js
+++ funciones.js
@@ -1,18 +1,13 @@
-// Supongamos que tienes dos etiquetas, una con la clase 'trigger' y otra con la clase 'target'
-// Quieres cambiar la propiedad 'color' de la etiqueta 'target' cuando el mouse está sobre 'trigger'
+var launcher = document.querySelector("#launcher");
+var take = document.querySelector(".evaluacion_viernes");
 
-// Primero, obtén la referencia de ambas etiquetas
-var trigger = document.querySelector(".trigger");
-var target = document.querySelector(".target");
+var give = document.querySelector(".evaluacion_info");
+var give_elements = give.innerHTML;
 
-// Luego, agrega un evento 'mouseover' a la etiqueta 'trigger'
-trigger.addEventListener('mouseover', function() {
-  // Cambia la propiedad 'color' de la etiqueta 'target'
-  target.style.color = 'rgb(193, 228, 228)';
+launcher.addEventListener("mouseover",function(){
+  give.innerHTML = take.innerHTML
 });
 
-// No olvides agregar un evento 'mouseout' para revertir el cambio cuando el mouse ya no esté sobre 'trigger'
-trigger.addEventListener('mouseout', function() {
-  // Revierte la propiedad 'color' de la etiqueta 'target'
-  target.style.color = 'initial';
+launcher.addEventListener("mouseout",function(){
+  give.innerHTML = give_elements
 });
