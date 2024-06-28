47eb2149fcc6c520a70f07dc02ae3360dfa3bbab
Samuel Rivero <jsamiju13@gmail.com>
2024-06-28 00:29:13 -0400

    Actualización mayor
    implementación de sweet alert, cambio de variables en js, cambio total de tamaño, ahora compatible con la mayoría de ordenadores, nuevo estilo

--- funciones.js
+++ funciones.js
@@ -1,62 +1,59 @@
 var launcher_organizacion_metodos = document.querySelector("#launcher_organizacion_metodos")
-var take_info_organizacion_metodos = document.querySelector(".caja_info_organizacion_metodos");
+var take_info_organizacion_metodos = document.querySelector(".caja_info_organizacion_metodos p");
 
 var launcher_lenguaje_programacion_martes = document.querySelector("#launcher_lenguaje_programacion_martes")
-var take_info_lenguaje_programacion_martes = document.querySelector(".caja_info_lenguaje_programacion_martes");
-
-
+var take_info_lenguaje_programacion_martes = document.querySelector(".caja_info_lenguaje_programacion_martes p");
 
 var launcher_ingles = document.querySelector("#launcher_ingles")
-var take_info_ingles = document.querySelector(".caja_info_ingles");
+var take_info_ingles = document.querySelector(".caja_info_ingles p");
 
 var launcher_sistemas_informacion = document.querySelector("#launcher_sistemas_informacion")
-var take_info_sistemas_informacion = document.querySelector(".caja_info_sistemas_informacion");
+var take_info_sistemas_informacion = document.querySelector(".caja_info_sistemas_informacion p");
 
 var launcher_actividades_complementarias_tecnologia = document.querySelector("#launcher_actividades_complementarias_tecnologia")
-var take_info_actividades_complementarias_tecnologia = document.querySelector(".caja_info_actividades_complementarias_tecnologia");
+var take_info_actividades_complementarias_tecnologia = document.querySelector(".caja_info_actividades_complementarias_tecnologia p");
 
 
 
 var launcher_lenguaje_programacion_jueves = document.querySelector("#launcher_lenguaje_programacion_jueves")
-var take_info_lenguaje_programacion_jueves = document.querySelector(".caja_info_lenguaje_programacion_jueves");
+var take_info_lenguaje_programacion_jueves = document.querySelector(".caja_info_lenguaje_programacion_jueves p");
 
 var launcher_matematica = document.querySelector("#launcher_matematica")
-var take_info_matematica = document.querySelector(".caja_info_matematica");
+var take_info_matematica = document.querySelector(".caja_info_matematica p");
 
 
 
 var launcher_estructura_datos = document.querySelector("#launcher_estructura_datos");
-var take_info_estructura_datos = document.querySelector(".caja_info_estructura_datos");
-
-
-
-var give_info_Información = document.querySelector(".caja_info_Información");
-var give_elements_original = give_info_Información.innerHTML;
+var take_info_estructura_datos = document.querySelector(".caja_info_estructura_datos p");
 
 var boton_click = document.querySelector("#sonido_boton");
 
 document.addEventListener('DOMContentLoaded', function() {
   var div = document.getElementById('estrella_1');
   div.addEventListener('click', function() {
-    alert('Agradecimientos especiales: \n Juan Lago');
   });
 });
 
 launcher_organizacion_metodos.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_organizacion_metodos.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_organizacion_metodos.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
 
 launcher_lenguaje_programacion_martes.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_lenguaje_programacion_martes.innerHTML
   Swal.fire({
-    title: "The Internet?",
-    text: "That thing is still around?",
-    icon: "question"
-  });
+    title: 'Información!',
+    text: take_info_lenguaje_programacion_martes.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
@@ -64,7 +61,12 @@ launcher_lenguaje_programacion_martes.addEventListener("click",function(){
 
 launcher_ingles.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_ingles.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_ingles.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
@@ -72,7 +74,12 @@ launcher_ingles.addEventListener("click",function(){
 
 launcher_sistemas_informacion.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_sistemas_informacion.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_sistemas_informacion.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
@@ -80,14 +87,24 @@ launcher_sistemas_informacion.addEventListener("click",function(){
 
 launcher_actividades_complementarias_tecnologia.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_actividades_complementarias_tecnologia.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_actividades_complementarias_tecnologia.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
 
 launcher_lenguaje_programacion_jueves.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_lenguaje_programacion_jueves.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_lenguaje_programacion_jueves.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
@@ -95,7 +112,12 @@ launcher_lenguaje_programacion_jueves.addEventListener("click",function(){
 
 launcher_matematica.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_matematica.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_matematica.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
@@ -103,7 +125,12 @@ launcher_matematica.addEventListener("click",function(){
 
 launcher_estructura_datos.addEventListener("click",function(){
   boton_click.play();
-  give_info_Información.innerHTML = take_info_estructura_datos.innerHTML
+  Swal.fire({
+    title: 'Información!',
+    text: take_info_estructura_datos.innerHTML,
+    icon: 'info',
+    confirmButtonText: 'Vale!'
+  })
 });
 
 
