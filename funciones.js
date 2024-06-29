f6047d24d8d3e63a544a2e6d06925aa20b76cc47
Samuel Rivero <jsamiju13@gmail.com>
2024-06-29 13:12:08 -0400

    Mejora de tamaño y alineación de íconos, progreso de paleta universal

--- funciones.js
+++ funciones.js
@@ -1,40 +1,32 @@
-var launcher_organizacion_metodos = document.querySelector("#launcher_organizacion_metodos")
+var launcher_organizacion_metodos = document.querySelector("#launcher_organizacion_metodos");
 var take_info_organizacion_metodos = document.querySelector(".caja_info_organizacion_metodos p");
 
-var launcher_lenguaje_programacion_martes = document.querySelector("#launcher_lenguaje_programacion_martes")
+var launcher_lenguaje_programacion_martes = document.querySelector("#launcher_lenguaje_programacion_martes");
 var take_info_lenguaje_programacion_martes = document.querySelector(".caja_info_lenguaje_programacion_martes p");
 
-var launcher_ingles = document.querySelector("#launcher_ingles")
+var launcher_ingles = document.querySelector("#launcher_ingles");
 var take_info_ingles = document.querySelector(".caja_info_ingles p");
 
-var launcher_sistemas_informacion = document.querySelector("#launcher_sistemas_informacion")
+var launcher_sistemas_informacion = document.querySelector("#launcher_sistemas_informacion");
 var take_info_sistemas_informacion = document.querySelector(".caja_info_sistemas_informacion p");
 
-var launcher_actividades_complementarias_tecnologia = document.querySelector("#launcher_actividades_complementarias_tecnologia")
+var launcher_actividades_complementarias_tecnologia = document.querySelector("#launcher_actividades_complementarias_tecnologia");
 var take_info_actividades_complementarias_tecnologia = document.querySelector(".caja_info_actividades_complementarias_tecnologia p");
 
-
-
-var launcher_lenguaje_programacion_jueves = document.querySelector("#launcher_lenguaje_programacion_jueves")
+var launcher_lenguaje_programacion_jueves = document.querySelector("#launcher_lenguaje_programacion_jueves");
 var take_info_lenguaje_programacion_jueves = document.querySelector(".caja_info_lenguaje_programacion_jueves p");
 
-var launcher_matematica = document.querySelector("#launcher_matematica")
+var launcher_matematica = document.querySelector("#launcher_matematica");
 var take_info_matematica = document.querySelector(".caja_info_matematica p");
-
-
 
 var launcher_estructura_datos = document.querySelector("#launcher_estructura_datos");
 var take_info_estructura_datos = document.querySelector(".caja_info_estructura_datos p");
 
+
 var boton_click = document.querySelector("#sonido_boton");
 
-document.addEventListener('DOMContentLoaded', function() {
-  var div = document.getElementById('estrella_1');
-  div.addEventListener('click', function() {
-  });
-});
 
-launcher_organizacion_metodos.addEventListener("click",function(){
+launcher_organizacion_metodos.addEventListener("click",function(){                                            
   boton_click.play();
   Swal.fire({
     title: 'Información!',
@@ -43,8 +35,6 @@ launcher_organizacion_metodos.addEventListener("click",function(){
     confirmButtonText: 'Vale!'
   })
 });
-
-
 
 launcher_lenguaje_programacion_martes.addEventListener("click",function(){
   boton_click.play();
@@ -132,5 +122,3 @@ launcher_estructura_datos.addEventListener("click",function(){
     confirmButtonText: 'Vale!'
   })
 });
-
-
