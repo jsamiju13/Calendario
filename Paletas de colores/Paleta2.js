6e7a40f4ce53e75468ec47909f713f07068529bd
Samuel Rivero <jsamiju13@gmail.com>
2024-06-30 11:02:03 -0400

    Actualización mayor, Paleta de colores

---
+++ Paletas de colores/Paleta2.js
@@ -0,0 +1,155 @@
+
+
+
+function constructor_colores2(){
+
+//PALETA DE COLORES----------------------------------------------------------------------------------------------------------
+
+    let backimg= 'linear-gradient(to bottom, #00C48F, #B9FFE1)';
+    let title = "#F8F8F8";
+    let diasBack = '#F8F8F8'
+    let diasBord = '#F8F8F8'
+    let h3Color = '#00927B'
+    let h4Color = "#000000"
+    let spanColor = "#00927B"
+    let buttonBord ='#00927B'
+    let buttonBordHover = "#1C1C1C"
+    let paleta9Color = "#1C1C1C"
+    //PALETA: GENERAL//
+  
+    let paleta_general = document.querySelector(".General");
+    paleta_general.style.backgroundImage = backimg;
+  
+    //--------------//
+  
+    //PALETA: TITULO//
+  
+    let paleta_titulo = document.querySelector("#titulo")
+    paleta_titulo.style.color = title;
+  
+    //--------------//
+  
+    //PALETA: SEMANA//
+  
+    let paleta_semana = document.querySelector("#semana")
+    paleta_semana.style.color = title;
+  
+    //--------------//
+  
+    //PALETA: DÍAS//
+  
+    let paleta_dia = document.querySelectorAll(".caja_dias .dia")
+    for(i=0; i< paleta_dia.length;i++){
+    paleta_dia[i].style.backgroundColor = diasBack;
+    paleta_dia[i].style.borderColor = diasBord;
+
+    }
+    //--------------//
+  
+    //PALETA: H3//
+  
+    let paleta_h3 = document.querySelectorAll("h3")
+    for(i=0;i<paleta_h3.length;i++){
+      paleta_h3[i].style.color = h3Color;
+    }
+    //--------------//
+  
+    //PALETA: HR//
+  
+    let paleta_hr = document.querySelectorAll("hr")
+  
+    for(i=0;i<paleta_hr.length;i++){
+      paleta_hr[i].style.borderTopColor = h3Color;
+      paleta_hr[i].style.borderBottomColor = h3Color;
+    }
+    //--------------//
+  
+    //PALETA: H4//
+  
+    //let paleta_h4 = document.querySelectorAll("h4")
+    //for(i=0;i<paleta_h4.length;i++){
+      //paleta_h4[i].style.color = "#fdb36a";
+    //}
+  
+    //--------------//
+  
+    //PALETA: SPAN//
+  
+    let paleta_span = document.querySelectorAll("span")
+    for(i=0;i<paleta_span.length;i++){
+      paleta_span[i].style.color = spanColor;
+    }
+    //--------------//
+  
+    //PALETA: BOTONES; FONDO, BORDES, TEXTO, HOVER//
+  
+    let paleta_button = document.querySelectorAll(".caja_dias button");
+    let paleta_button_h4 = document.querySelectorAll(".caja_dias button h4");
+    for (let i = 0; i < paleta_button.length; i++) {
+      paleta_button[i].style.backgroundColor = diasBack;
+      paleta_button[i].style.borderColor = buttonBord;
+      paleta_button_h4[i].style.color = h4Color;
+  
+      function handleMouseDown() {
+        paleta_button[i].style.borderColor = buttonBordHover;
+      }
+  
+      function handleMouseUp() {
+        paleta_button[i].style.borderColor = buttonBord;
+      }
+  
+      function hover_on() {
+        paleta_button_h4[i].style.color = h3Color;
+      }
+  
+      function hover_off(){
+        paleta_button_h4[i].style.color = h4Color;
+      }
+  
+      paleta_button[i].addEventListener("mousedown", handleMouseDown);
+      paleta_button[i].addEventListener("mouseup", handleMouseUp);
+      paleta_button_h4[i].addEventListener("mouseover", hover_on);
+      paleta_button_h4[i].addEventListener("mouseout", hover_off);
+    }
+  
+    //--------------//
+  
+    //PALETA: I//
+  
+  let paleta_i = document.querySelectorAll(".caja_dias i");
+  
+  for(i=0;i<paleta_i.length;i++){
+      paleta_i[i].style.color = h3Color;
+      }
+  
+  //--------------//
+  
+  //PALETA: A//
+  
+  let paleta_a = document.querySelectorAll("a");
+  
+  for(i=0;i<paleta_a.length;i++){
+      paleta_a[i].style.color = h3Color;
+      }
+  
+  
+  //--------------//
+
+  //PALETA: CAJA DE PALETAS, BOTONES DE PALETAS//
+
+  let paleta_menu_paletas = document.querySelector(".caja_paletas")
+  paleta_menu_paletas.style.backgroundColor = diasBack;
+  paleta_menu_paletas.style.borderColor = "transparent";
+  
+  let paleta_button_paleta = document.querySelectorAll(".caja_paletas button")
+  for(i=0;i<paleta_button_paleta.length;i++){
+    paleta_button_paleta[i].style.backgroundColor ="transparent";
+    paleta_button_paleta[i].style.borderColor = "transparent";
+  }
+
+  paleta_i_paleta9 = document.querySelector("#paleta9 i");
+  paleta_i_paleta9.style.color =  paleta9Color;
+
+  //--------------//
+
+  }
--- funciones.js
+++ funciones.js
@@ -1,3 +1,4 @@
+
 var launcher_organizacion_metodos = document.querySelector("#launcher_organizacion_metodos");
 var take_info_organizacion_metodos = document.querySelector(".caja_info_organizacion_metodos p");
 
@@ -24,7 +25,6 @@ var take_info_estructura_datos = document.querySelector(".caja_info_estructura_d
 
 
 var boton_click = document.querySelector("#sonido_boton");
-
 
 launcher_organizacion_metodos.addEventListener("click",function(){                                            
   boton_click.play();
@@ -122,3 +122,148 @@ launcher_estructura_datos.addEventListener("click",function(){
     confirmButtonText: 'Vale!'
   })
 });
+
+let button_paleta_1 = document.querySelector("#paleta1")
+button_paleta_1.addEventListener("click", function(){
+    boton_click.play();
+    constructor_colores();
+
+  });
+
+let button_paleta_2 = document.querySelector("#paleta2")
+button_paleta_2.addEventListener("click", function(){
+    boton_click.play();
+    constructor_colores2();
+
+  });
+  
+let button_paleta_3 = document.querySelector("#paleta3")
+button_paleta_3.addEventListener("click", function(){
+  boton_click.play();
+  constructor_colores3();
+
+});
+  
+let button_paleta_4 = document.querySelector("#paleta4")
+button_paleta_4.addEventListener("click", function(){
+    boton_click.play();
+    constructor_colores4();
+
+  });
+  
+let paleta_menu_paletas = document.querySelector(".caja_paletas")
+let paleta_menu_paletas_i = document.querySelectorAll(".caja_paletas i")
+let button_paleta_9 = document.querySelector("#paleta9")
+let paleta_active = 1
+
+button_paleta_9.addEventListener("click", function(){
+  boton_click.play();
+  
+  for (i=0; i<paleta_menu_paletas_i.length-1;i++){
+
+    paleta_menu_paletas_i[i].style.display = "initial"
+  }
+
+  if (paleta_active == 1) {
+    paleta_menu_paletas.style.left = "0%"
+    button_paleta_9.style.position = "absolute"
+    button_paleta_9.style.left = "89.5%"
+    button_paleta_9.style.top = "8%"
+    paleta_active=0
+  }else{
+
+    for (i=0; i<paleta_menu_paletas_i.length-1;i++){
+
+      paleta_menu_paletas_i[i].style.display = "none"
+    }
+    paleta_menu_paletas.style.left = "-81%"
+    button_paleta_9.style.position = "relative"
+    button_paleta_9.style.left = "auto"
+    button_paleta_9.style.top = "auto"
+    paleta_active=1
+  }
+  
+
+
+
+});
+
+let paleta1Color = "#e54059"
+let paleta2Color = "#00C48F"
+let paleta3Color = "#00FFFF"
+let paleta4Color = "#001BFF"
+let paleta5Color = "transparent"
+let paleta6Color = "transparent"
+let paleta7Color = "transparent"
+let paleta8Color = "transparent"
+let paleta9Color = "#000000"
+
+paleta_i_paleta1 = document.querySelector("#paleta1 i");
+paleta_i_paleta1.style.color =  paleta1Color;
+  
+  
+
+paleta_i_paleta2 = document.querySelector("#paleta2 i");
+paleta_i_paleta2.style.color =  paleta2Color;
+  
+  
+
+paleta_i_paleta3 = document.querySelector("#paleta3 i");
+paleta_i_paleta3.style.color =  paleta3Color;
+  
+  
+
+paleta_i_paleta4 = document.querySelector("#paleta4 i");
+paleta_i_paleta4.style.color =  paleta4Color;
+  
+  
+
+//paleta_i_paleta5 = document.querySelector("#paleta5 i");
+//paleta_i_paleta5.style.color =  paleta5Color;
+  
+//paleta_i_paleta2 = document.querySelector("#paleta6 i");
+//paleta_i_paleta2.style.color =  paleta6Color;
+  
+  
+
+//paleta_i_paleta3 = document.querySelector("#paleta7 i");
+//paleta_i_paleta3.style.color =  paleta7Color;
+  
+  
+
+//paleta_i_paleta4 = document.querySelector("#paleta8 i");
+//paleta_i_paleta4.style.color =  paleta8Color;
+  
+  
+
+paleta_i_paleta9 = document.querySelector("#paleta9 i");
+paleta_i_paleta9.style.color =  paleta9Color;
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
+if (getRandomInt(1,5)==1){
+  window.addEventListener("DOMContentLoaded", function(event) {
+    constructor_colores()
+  });
+}else{
+  if(getRandomInt(1,5)==2){
+    window.addEventListener("DOMContentLoaded", function(event) {
+      constructor_colores2()
+    });
+  }else{
+    if(getRandomInt(1,5)==3){
+      window.addEventListener("DOMContentLoaded", function(event) {
+        constructor_colores3()
+      });
+    }else{
+      window.addEventListener("DOMContentLoaded", function(event) {
+        constructor_colores4()
+      });
+    }
+  }
+}
+
