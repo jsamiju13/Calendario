afff3f169afb2fbe62101b2425c4799fe21efe4b
Samuel Rivero <jsamiju13@gmail.com>
2024-09-27 23:42:41 -0400

    npm to cdn

--- Paquetes/node_modules/sweetalert2/src/types.js
+++ Paquetes/node_modules/sweetalert2/src/types.js
@@ -1,6 +1,7 @@
 /**
  * @typedef { import('./SweetAlert').SweetAlert } SweetAlert
  * @typedef { import('sweetalert2').SweetAlertOptions } SweetAlertOptions
+ * @typedef { import('sweetalert2').SweetAlertCustomClass } SweetAlertCustomClass
  * @typedef { import('sweetalert2').SweetAlertIcon } SweetAlertIcon
  * @typedef { import('sweetalert2').SweetAlertInput } SweetAlertInput
  * @typedef { import('sweetalert2').SweetAlertResult } SweetAlertResult
@@ -9,6 +10,7 @@
 
 /**
  * @typedef { import('./utils/Timer').default } Timer
+ * @typedef { import('./utils/EventEmitter').default } EventEmitter
  */
 
 /**
@@ -16,6 +18,7 @@
  * @property {SweetAlert} [currentInstance]
  * @property {Element | null} [previousActiveElement]
  * @property {Timer} [timeout]
+ * @property {EventEmitter} [eventEmitter]
  * @property {NodeJS.Timeout} [restoreFocusTimeout]
  * @property {(this: HTMLElement, event: KeyboardEvent) => any} [keydownHandler]
  * @property {HTMLElement | (Window & typeof globalThis)} [keydownTarget]
@@ -36,4 +39,84 @@
  * @property {HTMLElement} closeButton
  * @property {HTMLElement} validationMessage
  * @property {HTMLElement} progressSteps
+ */
+
+/**
+ * @typedef
+ * { | 'container'
+ *   | 'shown'
+ *   | 'height-auto'
+ *   | 'iosfix'
+ *   | 'popup'
+ *   | 'modal'
+ *   | 'no-backdrop'
+ *   | 'no-transition'
+ *   | 'toast'
+ *   | 'toast-shown'
+ *   | 'show'
+ *   | 'hide'
+ *   | 'close'
+ *   | 'title'
+ *   | 'html-container'
+ *   | 'actions'
+ *   | 'confirm'
+ *   | 'deny'
+ *   | 'cancel'
+ *   | 'default-outline'
+ *   | 'footer'
+ *   | 'icon'
+ *   | 'icon-content'
+ *   | 'image'
+ *   | 'input'
+ *   | 'file'
+ *   | 'range'
+ *   | 'select'
+ *   | 'radio'
+ *   | 'checkbox'
+ *   | 'label'
+ *   | 'textarea'
+ *   | 'inputerror'
+ *   | 'input-label'
+ *   | 'validation-message'
+ *   | 'progress-steps'
+ *   | 'active-progress-step'
+ *   | 'progress-step'
+ *   | 'progress-step-line'
+ *   | 'loader'
+ *   | 'loading'
+ *   | 'styled'
+ *   | 'top'
+ *   | 'top-start'
+ *   | 'top-end'
+ *   | 'top-left'
+ *   | 'top-right'
+ *   | 'center'
+ *   | 'center-start'
+ *   | 'center-end'
+ *   | 'center-left'
+ *   | 'center-right'
+ *   | 'bottom'
+ *   | 'bottom-start'
+ *   | 'bottom-end'
+ *   | 'bottom-left'
+ *   | 'bottom-right'
+ *   | 'grow-row'
+ *   | 'grow-column'
+ *   | 'grow-fullscreen'
+ *   | 'rtl'
+ *   | 'timer-progress-bar'
+ *   | 'timer-progress-bar-container'
+ *   | 'scrollbar-measure'
+ *   | 'icon-success'
+ *   | 'icon-warning'
+ *   | 'icon-info'
+ *   | 'icon-question'
+ *   | 'icon-error'
+ * } SwalClass
+ */
+
+/**
+ * @typedef {(eventName: string) => void} EventHandler
+ * @typedef {EventHandler[]} EventHandlers
+ * @typedef {Record<string, EventHandlers>} Events
  */
